13108ea025166f25883dc82446cb6aa5a7fcfedf
fireSparrow <vorobyov794@gmail.com>
2017-11-16 00:26:00 +0300

    cw

--- parser.py
+++ parser.py
@@ -11,7 +11,7 @@ class BaseParser:
     def parse(self):
         return {}
 
-    def examine(self, content):
+    def _checkup(self, content):
         return True
 
 
@@ -19,29 +19,36 @@ class HtmlParser(BaseParser):
 
     def _grab_page(self):
         res = None
-        while not self.checkup(res):
-            print("Попытка...")
+        while not self._checkup(res):
             proxy = get_proxy() if self.proxy else None
             g = Grab(proxy=proxy)
             g.go(self.url)
             res = g.doc.select('.')
         return res
 
-    def checkup(self, content):
-        check = getattr(self, 'check', None)
+    def _checkup(self, content):
+        check = self.templates.get('check')
         if check and content:
             if content.select(check[0]):
-                return content.select(check[0]).text() == check[1]
+                return bool(content.select(check))
             else:
                 return False
         else:
             return bool(content)
 
-    def _extract(self, page):
-        return page.html()
+    def _extract_blocks(self, page):
+        tc = self.templates.get('container')
+        tb = self.templates.get('block')
+        return page.select(tc).select(tb)
+
+    def _extract_item(self, block):
+        pass
 
     def parse(self):
-        return self._extract(self._grab_page())
+        page = self._grab_page()
+        blocks = self._extract_blocks(page)
+        print(len(blocks))
+        return [self._extract_item(block) for block in blocks]
 
 
 
--- source.py
+++ source.py
@@ -1,3 +1,38 @@
 
+from parser import HtmlParser
+
+
 class BaseSourse:
-    pass
+
+    _parser = None
+    _params = {'proxy': False}
+    _templates = {}
+
+    def _get_parser(self, params):
+        return self._parser(**params, templates=self._templates)
+
+    def get_items(self):
+        parser = self._get_parser(self._params)
+        return parser.parse()
+
+
+class KinozalSearch(BaseSourse):
+
+    _parser = HtmlParser
+    _base_url = 'http://kinozal.tv/browse.php?s='
+    _templates = {
+        'container': '*//table[contains(@class, "t_peer")]',
+        'block': 'tr[contains(@class, "bg")]',
+        'check': '*/title[text()="Раздачи :: Кинозал.ТВ"]',
+    }
+    _params = {'proxy': 'True'}
+
+    def __init__(self, search):
+        params = self._params.copy()
+        params['url'] = self._base_url + search
+        self._params = params
+
+
+if __name__ == '__main__':
+    k = KinozalSearch(search='simpsons')
+    print(k.get_items())
